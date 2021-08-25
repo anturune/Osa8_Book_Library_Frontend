08dc6ff6c0d09600132b3cedec10d93150cd4d2b
Antti Turunen <asturune@hotmail.com>
2021-08-25 14:46:25 +0300

    kirjojen-listaus-mongosta-lisatty

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,23 +3,25 @@ import React from 'react'
 import { gql, useQuery, useLazyQuery } from '@apollo/client'
 
 //Haku backendiin "library-backend-Osa8"
+//author-kohtaa muutettu kun backendin skeema
+//muutettu siten, että Book -olion author kenttä on "author:Author!"
+//ja Book oliossa viitataan Authoriin vain ID:llä (ks. backend ja Book author: resolveri)
 const ALL_BOOKS = gql`
 query {
   allBooks { 
-      title 
-      author
+      title
+      author{name}
       published 
   }
 }
 `
-
 const Books = (props) => {
 
   //Tehdään haku backendiin
   //PollInterval päivittää välimuistit 2sek välein
-  const books = useQuery(ALL_BOOKS, { pollInterval: 2000 })
+  //const books = useQuery(ALL_BOOKS, { pollInterval: 2000 })
   //Jos ei halua pollausta, niin näin
-  //const books = useQuery(ALL_BOOKS)
+  const books = useQuery(ALL_BOOKS)
 
 
   //Kun painetaan muuta kuin "books" -nappia, niin propsina
@@ -56,7 +58,7 @@ const Books = (props) => {
           {books.data.allBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
-              <td>{a.author}</td>
+              <td>{a.author.name}</td>
               <td>{a.published}</td>
             </tr>
           )}
