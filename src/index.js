53f7fbfa61c158443ff391cafeebcef780ef861c
Antti Turunen <asturune@hotmail.com>
2021-09-06 14:54:30 +0300

    vähän myös websocket koodiakin

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './App'
 //käyttäjän token lähettämiseen pyynnön mukana
 import { setContext } from 'apollo-link-context'
-
+//"split" subscriptioneja varten
 import {
-    ApolloClient, ApolloProvider, HttpLink, InMemoryCache
+    ApolloClient, ApolloProvider, HttpLink, InMemoryCache, split
 } from '@apollo/client'
+
+//subscriptioneja varten
+import { getMainDefinition } from '@apollo/client/utilities'
+import { WebSocketLink } from '@apollo/client/link/ws'
 
 //client-olion muodostamisen yhteydessä oleva toinen parametri link määrittelee, 
 //miten apollo on yhteydessä palvelimeen. Nyt normaalia httpLink-yhteyttä muokataan siten, 
@@ -24,16 +28,42 @@ const authLink = setContext((_, { headers }) => {
 })
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+//Subscriptioneja varten webSocket
+//sovelluksella tulee nyt olla HTTP-yhteyden lisäksi websocket-yhteys GraphQL-palvelimelle
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:4000/graphql',
+    options: {
+        reconnect: true
+    }
+})
+//Sovelluksella tulee nyt olla HTTP-yhteyden lisäksi websocket-yhteys GraphQL-palvelimelle
+//"wsLink" ja "httpLink"
+const splitLink = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query)
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        )
+    },
+    wsLink,
+    authLink.concat(httpLink),
+)
+
 //Luodaan client olio, jolla kommunikoidaan palvelimelle ja voidaan
 //hakea mm. kaikki Authorsit
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: authLink.concat(httpLink)
+    //link: authLink.concat(httpLink)
+    //splitLink sisältää molemmat, http-linkin sekä websocket-linkin
+    link: splitLink
     /*
     link: new HttpLink({
         uri: 'http://localhost:4000'
         */
 })
+
 //kääritään komponentti App komponentin ApolloProvider lapseksi, jotta saadaan Client
 //kaikkien komponenttien saataville
 ReactDOM.render(
